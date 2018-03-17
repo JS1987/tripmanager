6e75689eac82af595e542adbc57aa1019bf394ed
student11 <student11@pcoip.ki.agh.edu.pl>
2018-03-17 12:12:22 +0100

    test

---
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -0,0 +1,22 @@
+package pl.edu.agh.mwo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Trip {
+
+	Collection <Photo> photos = new ArrayList <Photo>();
+	
+	public Collection<Photo> getPhotos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addPhoto(Photo photo) {
+		return;
+			
+	}
+
+
+	
+}
--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,5 +1,20 @@
 package pl.edu.agh.mwo;
- 
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TripManagerTest {
 	
+	@Test
+	public void addPhoto() {
+		Trip trip = new Trip();
+		Photo photo = new Photo();
+		assertEquals(0, trip.getPhotos().size());
+		trip.addPhoto(photo);
+		assertEquals(1, trip.getPhotos().size());
+	}
+	
+	@Test
+	public void 
 }
